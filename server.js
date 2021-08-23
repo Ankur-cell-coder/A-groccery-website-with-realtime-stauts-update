3b15d186c53e8d2981ecb3ff560ac4be2c50ef3e
Ankur Kumar <ankursingh6243@gmail.com>
2021-08-23 13:15:18 +0530

    make home page

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 3000
 
+//Assets
+app.use(express.static('public'))
+
 app.get('/', (req, res)=>{
     res.render('home')
 })
