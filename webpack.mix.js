6b1fee0ca92ed02ac1c30c68d7be15a71f241a38
Ankur Kumar <ankursingh6243@gmail.com>
2021-08-24 12:47:49 +0530

    add cart page,auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.scss');
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
 
 // Full API
 // mix.js(src, output);
